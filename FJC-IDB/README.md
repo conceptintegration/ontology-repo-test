ca81ae4a371654582e1f38a511e1e43082d96975
Andrés Cruz <andres.cruz@utexas.edu>
2025-07-25 15:39:11 -0300

    Add FJC-IDB

---
+++ FJC-IDB/README.md
@@ -0,0 +1,12 @@
+# Federal Judicial Center - Integrated Database (FJC-IDB)
+
+- **Description**: The IDB contains data on civil case and criminal defendant filings and terminations in the district courts, along with bankruptcy court and appellate court case information from 1970 to the present.
+
+- **Citation**: Federal Judicial Center. 2016. "Appeals Integrated Database (IDB): 1971 to 2007, Codebook." Available at https://www.fjc.gov/sites/default/files/idb/codebooks/Appeals%20Codebook%201971-2007.pdf.
+- **Citation**: Federal Judicial Center. 2023. "Bankruptcy Petition NewSTATS Snapshots Database, Codebook." Available at https://www.fjc.gov/sites/default/files/idb/codebooks/Bankruptcy%20IDB%20Online%20Codebook%20rev%2002282023.pdf.
+- **Citation**: Federal Judicial Center. 1988. "Integrated Data Base: Civil Codebook." Available at https://www.fjc.gov/sites/default/files/idb/codebooks/Civil Codebook 1970-1987.pdf.
+- **Citation**: Federal Judicial Center. 1996. "Criminal Integrated Database (IDB): 1970-1995, Codebook." Available at https://www.fjc.gov/sites/default/files/idb/codebooks/Criminal%20Code%20Book%201970-1995.pdf.
+
+- **Version (in the repo)**: 1
+---
+(This `README.md` file is automatically generated from the `metadata.csv` file. Edit that one!)
--- process.R
+++ process.R
@@ -4,18 +4,32 @@ fps_metadata <- list.files(pattern = "metadata\\.csv$", recursive = T)
 
 for (fp in fps_metadata){
   md <- read.csv(fp)
+  colnames(md) <- stringr::str_to_lower(colnames(md))
 
-  out_lines <- c(
-    sprintf("# %s - %s (%s)", md$organization, md$name, md$label),
-    "",
-    sprintf("- **Description**: %s", md$description),
-    "",
-    sprintf("- **Citation**: %s", md$citation),
-    "",
-    sprintf("- **Version (in the repo)**: %s", md$version),
-    "---",
-    "(This `README.md` file is automatically generated from the `metadata.csv` file. Edit that one!)"
-  )
-
+  if (nrow(md) == 1L){
+    out_lines <- c(
+      sprintf("# %s - %s (%s)", md$organization, md$name, md$label),
+      "",
+      sprintf("- **Description**: %s", md$description),
+      "",
+      sprintf("- **Citation**: %s", md$citation),
+      "",
+      sprintf("- **Version (in the repo)**: %s", md$version),
+      "---",
+      "(This `README.md` file is automatically generated from the `metadata.csv` file. Edit that one!)"
+    )
+  } else if (nrow(md) > 1L) {
+    out_lines <- c(
+      sprintf("# %s - %s (%s)", unique(md$organization), unique(md$nameone), unique(md$label)),
+      "",
+      sprintf("- **Description**: %s", unique(md$description)),
+      "",
+      sprintf("- **Citation**: %s", md$citation),
+      "",
+      sprintf("- **Version (in the repo)**: %s", unique(md$version)),
+      "---",
+      "(This `README.md` file is automatically generated from the `metadata.csv` file. Edit that one!)"
+    )
+  }
   writeLines(out_lines, sub("metadata\\.csv", "README.md", fp))
 }
